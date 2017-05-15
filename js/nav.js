9d968a477fc882ceafbdbe25b83f90dac4810a37
msm3921 <msm3921@yahoo.com>
2017-05-15 07:26:43 -0400

    changed everything
    changed everything

---
+++ js/nav.js
@@ -0,0 +1,12 @@
+$('.document').ready(function(){
+  
+$('.navlist img').click(showNav);
+  
+  function showNav(event){
+    event.preventDefault();
+    
+   $('.navOptions').toggleClass('hideMe');
+    console.log('navShow');
+  }
+})
+
--- js/tickets.js
+++ js/tickets.js
@@ -9,25 +9,25 @@ $('.ticketHeader li').click(showType);
 
 
 function showType(event){
-	event.preventDefault();
+  event.preventDefault();
 
-	var getType = $(this).attr('class');
+  var getType = $(this).attr('class');
  
       if (getType !== 'gaButton' & $('.gaInfo').hasClass('reset')){
-      $('.gaInfo>.ticketInfo').animate({left: "+=50%" }, 200,'easeOutBounce');
-      $('.gaInfo>.ticketImage').animate({left: "-=75%" }, 200,'easeOutBounce');
+      $('.gaInfo>.ticketImage').animate({left: "+=22%" }, 200,'easeOutBounce');
+      $('.gaInfo>.ticketInfo').animate({left: "-=70%" }, 200,'easeOutBounce');
       $('.gaInfo').removeClass('reset');
 }
   
       if (getType !== 'vipButton' & $('.vipInfo').hasClass('reset')){
-      $('.vipInfo>.ticketInfo').animate({left: "+=50%" }, 200,'easeOutBounce');
-      $('.vipInfo>.ticketImage').animate({left: "-=75%" }, 200,'easeOutBounce');
+      $('.vipInfo>.ticketImage').animate({left: "+=22%" }, 200,'easeOutBounce');
+      $('.vipInfo>.ticketInfo').animate({left: "-=70%" }, 200,'easeOutBounce');
       $('.vipInfo').removeClass('reset');
 }   
   
       if (getType !== 'superButton' & $('.superVIPInfo').hasClass('reset')){
-      $('.superVIPInfo>.ticketInfo').animate({left: "+=50%" }, 200,'easeOutBounce');
-      $('.superVIPInfo>.ticketImage').animate({left: "-=75%" }, 200,'easeOutBounce');
+      $('.superVIPInfo>.ticketImage').animate({left: "+=22%" }, 200,'easeOutBounce');
+      $('.superVIPInfo>.ticketInfo').animate({left: "-=70%" }, 200,'easeOutBounce');
       $('.superVIPInfo').removeClass('reset');
 }
   
@@ -35,41 +35,41 @@ if ((getType === 'gaButton')&$('.gaInfo').hasClass('active')){
    console.log('nothing');
 }
  else if (getType === 'gaButton'){
-		console.log('GA');
-    $('.gaInfo').addClass('reset active');
-    $('.vipInfo').hide();
-    $('.superVIPInfo').hide();
-    $('.gaInfo').show();
-		$('.gaInfo>.ticketInfo').animate({left: "-=50%" }, 1200,'easeOutBounce');
-    $('.gaInfo>.ticketImage').animate({left: "+=75%" }, 1200,'easeOutBounce');
+    console.log('GA');
+     $('.gaInfo').addClass('reset active');
+     $('.vipInfo').hide();
+     $('.superVIPInfo').hide();
+     $('.gaInfo').show();
+     $('.gaInfo>.ticketImage').animate({left: "-=22%" }, 1200,'easeOutBounce');
+   $('.gaInfo>.ticketInfo').animate({left: "+=70%" }, 1200,'easeOutBounce');
     $('.vipInfo').removeClass('active');
     $('.superVIPInfo').removeClass('active');
-	}
+  }
 else if ((getType === 'vipButton')&$('.vipInfo').hasClass('active')){
    console.log('nothing');
 }
-		else if (getType === 'vipButton'){
-		  console.log('VIP');
+    else if (getType === 'vipButton'){
+      console.log('VIP');
      $('.vipInfo').addClass('reset active');
      $('.gaInfo').hide();
      $('.superVIPInfo').hide();
      $('.vipInfo').show();
-     $('.vipInfo>.ticketInfo').animate({left: "-=50%" }, 1200,'easeOutBounce');
-     $('.vipInfo>.ticketImage').animate({left: "+=75%" }, 1200,'easeOutBounce');
+     $('.vipInfo>.ticketImage').animate({left: "-=22%" }, 1200,'easeOutBounce');
+     $('.vipInfo>.ticketInfo').animate({left: "+=70%" }, 1200,'easeOutBounce');
      $('.gaInfo').removeClass('active');
      $('.superVIPInfo').removeClass('active');
-		}
+    }
  else if ((getType === 'superButton')&$('.superVIPInfo').hasClass('active')){
    console.log('nothing');
 }
-		else if (getType === 'superButton'){
-			console.log('super');
+    else if (getType === 'superButton'){
+      console.log('super');
       $('.superVIPInfo').addClass('reset active');
       $('.gaInfo').hide();
       $('.vipInfo').hide();
       $('.superVIPInfo').show();
-		  $('.superVIPInfo>.ticketInfo').animate({left: "-=50%" }, 1200,'easeOutBounce');
-      $('.superVIPInfo>.ticketImage').animate({left: "+=75%" }, 1200,'easeOutBounce');
+      $('.superVIPInfo>.ticketImage').animate({left: "-=22%" }, 1200,'easeOutBounce');
+      $('.superVIPInfo>.ticketInfo').animate({left: "+=70%" }, 1200,'easeOutBounce');
       $('.gaInfo').removeClass('active');
       $('.vipInfo').removeClass('active');
     }
